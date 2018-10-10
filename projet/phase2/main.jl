6c46748ee5df5fe1af0fde93d566e86ff37aa844
Samuel Goyette <samuel.goyette@usherbrooke.ca>
2018-10-10 19:24:07 -0400

    Ajout de documentation

--- projet/phase2/main.jl
+++ projet/phase2/main.jl
@@ -1,7 +1,12 @@
-include("test-connected.jl")
-include("exemple-laboratoire.jl")
+# On va chercher le graphe créer par  le main de la phase 1
+include("../phase1/main.jl")
+
+# On va chercher l'algorithme de Kruskal et les nouvelles structures
+# et fonctions qu'on a définie
+cd("../phase2/")
+include("composante_connexe.jl")
 include("kruskal.jl")
 
-A = kruskal(exemple_graph)
-
-show(A)
+# On construit l'arbre de recouvrement minimal avec l'algorithme de kruskal
+A₂ = kruskal(G)
+show(A₂)
