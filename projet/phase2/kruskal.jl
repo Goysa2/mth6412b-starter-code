1ea7d3e46055fa39fd5902c05b3d6a7c15606c53
Samuel Goyette <samuel.goyette@usherbrooke.ca>
2018-10-10 19:23:53 -0400

    enlever des commentaires de debogage

--- projet/phase2/kruskal.jl
+++ projet/phase2/kruskal.jl
@@ -53,14 +53,6 @@ function kruskal(G :: Graph{T}) where T
             no_merge && add_node!(Aₖ, edge.node1)
             no_merge && add_edge!(Aₖ, edge)
         end
-        # fonction qui m'ont aidé à débuger, ultimement on devrait les enlever
-        # println("à l'itération $k  on a Aₖ")
-        # show(Aₖ)
-        # println(" ")
-        # println("à l'itération $k  on a temp_comp_conn")
-        # for cmp in temp_comp_conn
-        #     show(cmp)
-        # end
         k += 1
     end # for
 
