ecb1325106277a7bf7c3d1d85a52676ce8f0116e
Samuel Goyette <samuel.goyette@usherbrooke.ca>
2018-11-18 01:22:46 -0500

    on montre peut afficher une tournée avec RSL, mais pas son poids

--- projet/phase4/rsl.jl
+++ projet/phase4/rsl.jl
@@ -1,24 +1,14 @@
 function rsl(G :: Graph; algorithm_mst :: Function = kruskal2)
     A, racine = algorithm_mst(G)
-    edges_graph = edges(G)
-    nodes_graph = nodes(G)
 
-    edges_mst = edges(A)
-    nodes_mst = nodes(A)
-#    set_left!(racine, edges_mst[1].node1)
+    for node in nodes(A)
+        if parent(node) != node
+            add_children!(parent(node), node)
+        end
+    end
 
-#    TSP = Graph("TSP approximé", nodes_graph, edges_mst[1]) # On intialise le graphe représentant la tournée
-
-# Attibuer des enfants de gauche ou de droite aux noeuds du MST
-#    for nodey in nodes_mst
-#        for nodex in nodes_mst[2:end]
-#            if nodex.parent == nodey && nodey.left == nothing && nodey.right == nothing
-#                set_left!(nodey, nodex)
-#            elseif nodex.parent == nodey && nodey.left !== nothing
-#                set_right!(nodey, nodex)
-#            end
-#        end
-#    end
+    parcours_preodre(racine)
+    println(" ")
 
 return A
 end #function
