6075548133e67f456f07fa7b6983c59d4fa07e6d
Samuel Goyette <samuel.goyette@usherbrooke.ca>
2018-11-18 01:21:41 -0500

    modification des fonctions main

--- projet/phase2/main.jl
+++ projet/phase2/main.jl
@@ -1,11 +1,11 @@
 # On va chercher le graphe créer par  le main de la phase 1
- include("../phase1/main.jl")
+ # include("../phase1/main.jl")
 
 # Si on veut tester avec l'exemple du cours
-#include("../phase1/node.jl")
-#include("../phase1/edge.jl")
-#include("../phase1/graph.jl")
-#include("exemple-laboratoire.jl")
+include("../phase1/node.jl")
+include("../phase1/edge.jl")
+include("../phase1/graph.jl")
+include("exemple-laboratoire.jl")
 
 # On va chercher l'algorithme de Kruskal et les nouvelles structures
 # et fonctions qu'on a définie
@@ -13,6 +13,7 @@ cd("../phase2/")
 include("kruskal2.jl")
 
 # On construit l'arbre de recouvrement minimal avec l'algorithme de kruskal
- A₂, Root = kruskal2(G)
- show(A₂)
- show(Root)
+A₂, Root = kruskal2(G)
+
+show(A₂)
+show(Root)
--- projet/phase4/main.jl
+++ projet/phase4/main.jl
@@ -1,20 +1,26 @@
 # On va chercher le graphe créer par  le main de la phase 1
- include("../phase1/main.jl")
+ # include("../phase1/main.jl")
 
 
  # Si on veut tester avec l'exemple du cours
-# include("../phase1/node.jl")
-# include("../phase1/edge.jl")
-# include("../phase1/graph.jl")
-# include("../phase2/exemple-laboratoire.jl")
+include("../phase1/node.jl")
+include("../phase1/edge.jl")
+include("../phase1/graph.jl")
+include("../phase2/exemple-laboratoire.jl")
 
- # On va chercher l'algorithme de Kruskal
-  include("../phase2/kruskal2.jl")
+# On va chercher l'algorithme de Kruskal
+include("../phase2/kruskal2.jl")
+
+# On va chercher l'algorithme de Prim
+include("../phase3/prim.jl")
+
+#la fonction de parcours en pre ordre
+include("parcours-preodre.jl")
 
 # On va chercher l'algorithme de RSL
-cd("../phase4/")
 include("rsl.jl")
 
-# On construit l'arbre de recouvrement minimal avec l'algorithme de kruskal
- A₂ = rsl(G)
- show(A₂)
+# On cherche une tournée minimale et son poids
+println("la tournée donnée par l'algorithme RSL est: ")
+A₂ = rsl(G);
+println("Fin!")
