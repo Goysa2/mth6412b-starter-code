f3e88b0fe1e4fa89e3cf47cc0d3daa509367175a
Samuel Goyette <samuel.goyette@usherbrooke.ca>
2018-11-20 17:19:46 -0500

    plus de chose dans l'affichage et les valeurs de retour

--- projet/phase4/main-hk.jl
+++ projet/phase4/main-hk.jl
@@ -1,3 +1,6 @@
+# On a besoin de la fonction norm pour le critère d'arrêt de l'algorithme HK
+using LinearAlgebra
+
 # On va chercher le graphe créer par  le main de la phase 1
 include("../phase1/main.jl")
 
@@ -21,12 +24,17 @@ include("../phase3/prim.jl")
 include("parcours-preodre.jl")
 
 
-# On va chercher l'algorithme de RSL
+# On va chercher l'algorithme de HK
+include("step_size.jl")
 include("hk.jl")
 
 # On cherche une tournée minimale et son poids
 println("le 1-tree donné par les deux premières étapes de HK est: ")
-A, w, W, d = hk(G)#; algorithm_mst = prim);
-show(A)
-println("on a wᵏ = $w et W = $W et d = $d")
+A, w, W, d, v, t, π1 = hk(G; algorithm_mst = prim);
+# show(A)
+# println("on a wᵏ = $w et W = $W et d = $d")
+# println("d  = $d ")
+# println("v  = $v ")
+# println("t  = $t ")
+# println("π1 = $π1")
 println("Fin!")
