25ed48fb177824ee23c3ef5ba6f52f21aa783bad
Samuel Goyette <samuel.goyette@usherbrooke.ca>
2018-11-27 11:49:27 -0500

    enlève les break pour remplacer par while

--- projet/phase4/rsl.jl
+++ projet/phase4/rsl.jl
@@ -1,6 +1,8 @@
 function rsl(G :: Graph; algorithm_mst :: Function = kruskal2)
     A = algorithm_mst(G)
-    racine = nodes(A)[1]
+    n = length(nodes(A))
+    n_rand = rand(1:n)
+    racine = nodes(A)[n_rand]
     while root(racine) != racine
         racine = parent(racine)
     end
@@ -8,7 +10,7 @@ function rsl(G :: Graph; algorithm_mst :: Function = kruskal2)
 
     for node in nodes(A)
         if parent(node) != node
-                add_children!(parent(node), node)
+            add_children!(parent(node), node)
         end
     end
 
@@ -17,23 +19,27 @@ function rsl(G :: Graph; algorithm_mst :: Function = kruskal2)
     println(" ")
     poids_tournee = 0
     for i = 1 : length(edges(A))-1
-        node_1 = ordre_noeud[i]
-        node_2 = ordre_noeud[i+1]
-        for edge in edges(G)
+        node_1 = ordre_noeud[i]; node_2 = ordre_noeud[i+1]
+        edge_cpt = 1; new_weight = false
+        while !new_weight
+            edge = edges(G)[edge_cpt]
             if (edge.node1 == node_1 && edge.node2 == node_2) || (edge.node2 == node_1 && edge.node1 == node_2)
                 poids_tournee += weight(edge)
-                break
+                new_weight = true
             end
+            edge_cpt += 1
         end
     end
 
-    node_1 = ordre_noeud[1]
-    node_end = ordre_noeud[end]
-    for edge in edges(G)
+    node_1 = ordre_noeud[1]; node_end = ordre_noeud[end]
+    edge_cpt = 1; final_edge = false
+    while !final_edge
+        edge = edges(G)[edge_cpt]
         if (edge.node1 == node_1 && edge.node2 == node_end) || (edge.node2 == node_1 && edge.node1 == node_end)
             poids_tournee += weight(edge)
-            break
+            final_edge = true
         end
+        edge_cpt += 1
     end
 
 return A, poids_tournee
